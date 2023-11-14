d435b55a4e71d4a4f34c3b311d5d76bcb0daaa97
Mariano Jurado <mariano.e.jurado@gmail.com>
2023-11-14 15:52:56 -0300

    cambios ejecutados

--- app.js
+++ app.js
@@ -18,10 +18,15 @@ app.get("/login",(req, res)=>{
   res.sendFile(path.resolve("./views/login.html"))
 })
 
+app.get("/home",(req, res)=>{
+  res.sendFile(path.resolve("./views/home.html"))
+})
+
 
 app.listen(3000, () => {
   console.log("servidor funcionando...");
 });
 
-
+const PORT =process.env.PORT ||3001;
+app.listen(PORT,()=>console.log(`servidor corriendo en el puerto ${PORT}`))
 
